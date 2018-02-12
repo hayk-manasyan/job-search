17064a3908a9c5b0e96420aabf5a767195546882
Hayk Manasyan <hayk.a.manasyan@gmail.com>
2018-02-12 18:42:14 +0400

    - Added user module

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,6 +12,10 @@
  */
 
 use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
+use Zend\Session\Storage\SessionArrayStorage;
+use Zend\Session\Validator\RemoteAddr;
+use Zend\Session\Validator\HttpUserAgent;
+
 return [
     'doctrine' => [
         'connection' => [
@@ -33,5 +37,22 @@ return [
             'namespace' => 'Migrations',
             'table'     => 'migrations',
         ],
+    ],
+    // Session configuration.
+    'session_config' => [
+        'cookie_lifetime'     => 60*60*1, // Session cookie will expire in 1 hour.
+        'gc_maxlifetime'      => 60*60*24*30, // How long to store session data on server (for 1 month).
+    ],
+    // Session manager configuration.
+    'session_manager' => [
+        // Session validators (used for security).
+        'validators' => [
+            RemoteAddr::class,
+            HttpUserAgent::class,
+        ]
+    ],
+    // Session storage configuration.
+    'session_storage' => [
+        'type' => SessionArrayStorage::class
     ],
 ];
